cdb07e6c067411b6bdd836d305f23e6817f66406
ShuvoMallick01 <shuvacse01@gmail.com>
2023-02-13 14:20:20 +0600

    Complete funtionalize the slider

--- js/app.js
+++ js/app.js
@@ -10,11 +10,35 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+    // enabled: false,
   },
 
   // pagination
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
+  },
+
+  // Responsive
+  breakpoints: {
+    // when window width is >= 320px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
   },
 });
