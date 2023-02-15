d58ecdfaff9f265ea3cff83f4757cbf909b1feda
ShuvoMallick01 <shuvacse01@gmail.com>
2023-02-15 20:45:54 +0600

    Fixed the slider issue

--- js/app.js
+++ js/app.js
@@ -1,3 +1,67 @@
+const swiperContainers = document.querySelectorAll(".swiper-container");
+const swipers = document.querySelectorAll(".swiper");
+
+function createElement(className) {
+  const element = document.createElement("div");
+  element.className = className;
+  return element;
+}
+
+for (let i = 0; i < swiperContainers.length; i++) {
+  const slider = swiperContainers[i];
+
+  const prevButton = createElement("swiper-button-prev");
+  const nextButton = createElement("swiper-button-next");
+  const pagination = createElement("swiper-pagination");
+
+  slider.append(prevButton, nextButton, pagination);
+
+  const swiperEl = slider.querySelector(".swiper");
+  // console.log(swiperEl);
+
+  const swiper = new Swiper(swiperEl, {
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: slider.querySelector(".swiper-button-next"),
+      prevEl: slider.querySelector(".swiper-button-prev"),
+    },
+
+    // pagination
+    pagination: {
+      el: slider.querySelector(".swiper-pagination"),
+      type: "bullets",
+      clickable: true,
+    },
+
+    // Responsive
+    breakpoints: {
+      // when window width is >= 320px
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+}
+
+/**
 const swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   spaceBetween: 20,
@@ -41,3 +105,4 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+ */
