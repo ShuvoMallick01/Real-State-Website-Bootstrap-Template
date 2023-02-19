a3d6bbcee753abedaf6453cc15eb424fc8136174
ShuvoMallick01 <shuvacse01@gmail.com>
2023-02-20 01:12:12 +0600

    Add GLight Box Plugin for Apartments Gallery View

--- js/single-property-v1.js
+++ js/single-property-v1.js
@@ -2,5 +2,13 @@
 // ---------------------
 // GLIGHT BOX FOR IMAGE GELLERY
 
-const lightbox = GLightbox();
-console.log(lightbox);
+const lightbox = GLightbox({
+  touchNavigation: true,
+  loop: true,
+  openEffect: "fade",
+  closeEffect: "zoom",
+  slideEffect: "zoom",
+  moreLength: 60,
+  zoomable: true,
+  width: "1200px",
+});
