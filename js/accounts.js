4eee9d681d153cd7b0c4620a69e9b3257e23933a
ShuvoMallick01 <shuvacse01@gmail.com>
2023-02-21 00:20:50 +0600

    Add Password visible Function

--- js/accounts.js
+++ js/accounts.js
@@ -1,31 +1,18 @@
-// const dropzone = new Dropzone("div.my-dropzone", { url: "/assets/upload/" });
-Dropzone.options.myDropzone = {
-  // Configuration options go here
-};
+// PICTURE UPLOAD AREA
+Dropzone.options.myDropzone = {};
 
-// ACCOUNTS & SECURITY JS
-let eyePassword = document.querySelectorAll('[id="eye-password"]');
-// let typePassword = document.getAttribute("password");
-let passwordInput = document.querySelectorAll('[id="password"]');
+// PASSWORD FILED EYE ICON OFF ON
+const passwordfield = document.querySelectorAll(".form-input-password");
 
-// console.log(passwordInput);
+passwordfield.forEach((item) => {
+  const passwordInput = item.querySelector("#password");
+  const passwordIcon = item.querySelector("#eye-password");
 
-eyePassword.forEach((item) => {
-  // console.log(item);
-  item.addEventListener("click", (e) => {
-    item.classList.toggle("fa-eye-slash");
+  passwordIcon.addEventListener("click", (e) => {
+    passwordIcon.classList.toggle("fa-eye-slash");
 
-    console.log(e.target);
-    passwordInput.forEach((item) => {
-      const type =
-        item.getAttribute("type") === "password" ? "text" : "password";
-      item.setAttribute("type", type);
-    });
+    const type =
+      passwordInput.getAttribute("type") === "password" ? "text" : "password";
+    passwordInput.setAttribute("type", type);
   });
 });
-
-// function getAttributeFunction(attribute) {
-//   const type =
-//     passwordInput.getAttribute("type") === "password" ? "text" : "password";
-//   passwordInput.setAttribute("type", type);
-// }
